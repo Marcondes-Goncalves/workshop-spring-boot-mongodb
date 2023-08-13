9272ffe0aa73f6d8f2e8a3e321678366f05454af
Marcondes-Goncalves <marcondesgoncalves159@gmail.com>
2023-08-13 13:48:42 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/marcondesweb/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/marcondesweb/workshopmongo/repository/UserRepository.java
@@ -1,11 +1,8 @@
 package com.marcondesweb.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
 import com.marcondesweb.workshopmongo.domain.User;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     
 
